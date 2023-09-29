f9acd154d76bfaaeaee0e8870089470903d9a306
wayne tsao <a578ff6@gmail.com>
2023-09-29 18:30:34 +0800

    Initial Commit

---
+++ VC_Life_Cycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VC_Life_Cycle
+//
+//  Created by 曹家瑋 on 2023/9/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
